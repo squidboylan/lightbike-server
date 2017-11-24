85a3b251a35c78b8855323d5307409bc91ff818c
Caleb Boylan <calebboylan@gmail.com>
2017-11-24 15:20:45 -0800

    Fix problems

--- server/gameserver.py
+++ server/gameserver.py
@@ -16,32 +16,34 @@ import os
 # <TOKEN> DIRECTION <DIRECTION>
 
 class GameServer(DatagramProtocol):
+    def __init__(self):
+        self.token_length = 512/8
+        self.curr_game = None
 
     def datagramReceived(self, data, (host, port)):
         print "received %r from %s:%d" % (data, host, port)
         #self.transport.write(data, (host, port))
         self.parse_received_packet(data, (host, port))
-        self.token_length = 512/8
 
     def parse_received_packet(self, data, (host, port)):
         split_data = data.rstrip().split()
         if split_data[0] == "CREATE":
-            self.create_game(split_data)
+            self.create_game(split_data, (host, port))
         if split_data[0] == "AUTH":
-            self.auth(split_data, host, port)
+            self.auth(split_data, (host, port))
 
-    def create_game(self, split_data):
+    def create_game(self, split_data, (host, port)):
         print "Creating game of size " + split_data[1]
-        if not curr_game:
+        if not self.curr_game:
             self.curr_game = Game(int(split_data[1]))
-            send_str = "CREATE SUCCESS"
+            send_str = "CREATE SUCCESS\n"
             self.transport.write(send_str, (host, port))
         else:
             # Error 1 is game already exists
-            send_str = "CREATE ERROR 1"
+            send_str = "CREATE ERROR 1\n"
             self.transport.write(send_str, (host, port))
 
-    def auth(self, split_data, host, port):
+    def auth(self, split_data, (host, port)):
         print "User authenticating " + split_data[1]
         token = binascii.hexlify(os.urandom(self.token_length))
         self.curr_game.players[split_data[1]] = {}
